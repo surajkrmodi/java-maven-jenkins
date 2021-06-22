9d0086cd561b9378cec697d49e994ead9f6e43e3
Suraj Kumar <suraj.modi@publicissapient.com>
2021-06-22 16:13:01 +0530

    First commit

---
+++ src/main/java/com/infosys/product1/Application.java
@@ -0,0 +1,18 @@
+package com.infosys.product1;
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+	static final Logger logger = LoggerFactory.getLogger(Application.class);
+	public static void main(String[] args) {
+		//System.out.println("Hello Maven ");
+		logger.debug("This is debug message");
+		logger.info("This is info message");
+		logger.warn("This is warning message");
+		logger.error("This is error message");
+	}
+
+}
